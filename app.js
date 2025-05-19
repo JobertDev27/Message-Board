9c5d2ea01e9330451fb69dd4a2d11c85af505cb9
JobertDev27 <jobertdev27@gmail.com>
2025-05-19 15:14:44 +0800

    fix indexing issue with message router

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ import express from "express";
 import indexRouter from "./routes/indexRouter.js";
 import postRouter from "./routes/postRouter.js";
 import messageRouter from "./routes/messageRouter.js";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-const __dirname = import.meta.dirname;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const assetPath = path.join(__dirname, "public");
 
 app.use(express.static(assetPath));
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -4,9 +4,9 @@ import messages from "../messagesDB.js";
 const router = express.Router();
 
 router.get("/:messageIndex", (req, res) => {
-  const index = parseInt(req.params.messageIndex, 10);
+  const id = parseInt(req.params.messageIndex, 10);
 
-  res.render("messagePage", { message: messages[Number(index)] });
+  res.render("messagePage", { message: messages.find((msg) => msg.id === id) });
 });
 
 export default router;
